ebf84d3e599075bf285a0fd0c6fc2a04d2ce2b63
bobyeong2 <bobyeong2@naver.com>
2021-02-14 18:00:51 +0900

    projectapp/view.py midified

--- projectapp/views.py
+++ projectapp/views.py
@@ -41,16 +41,14 @@ class ProjectDetailView(DetailView, MultipleObjectMixin):
     form_class = CommentCreationForm
     context_object_name = 'target_project'
     template_name = 'projectapp/detail.html'
-
     paginate_by = 25
-
 
     def get_context_data(self, **kwargs):
 
         project = self.object
         user = self.request.user
 
-        if user.is_authenticated :
+        if user.is_authenticated:
             subscription = Subscription.objects.filter(user=user,
                                                        project=project)
 
