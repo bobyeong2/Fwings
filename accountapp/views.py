8703389397ed799798f1b8c7666671b6adbf6192
bobyeong2 <bobyeong2@naver.com>
2021-02-14 20:05:02 +0900

    django error modify

--- accountapp/views.py
+++ accountapp/views.py
@@ -21,7 +21,7 @@ class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:login')
-    template_name = 'accountapp/detail.html'
+    template_name = 'accountapp/create.html'
 
 
 class AccountDetailView(DetailView, MultipleObjectMixin):
